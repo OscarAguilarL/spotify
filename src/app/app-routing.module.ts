ba7a6e7bfc54021d4ef3536f514f5a1f6e722fd5
Oscar Aguilar <aguilop.oscar@gmail.com>
2021-11-02 17:36:26 -0600

    Modulos con rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ExampleComponent } from './example/example.component';
+
+const routes: Routes = [
+    {
+        path: '',
+        component: ExampleComponent,
+    },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
