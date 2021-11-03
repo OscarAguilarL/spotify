ac0da3f8b314dfbfae851012b7161919e92c5813
Oscar Aguilar <aguilop.oscar@gmail.com>
2021-11-03 03:22:19 -0600

    Modulo compartido

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ExampleComponent } from './example/example.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ExampleComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        //! components, directives, pipes, declarations
+        AppComponent,
+    ],
+    imports: [
+        //! Solo se importan modules
+        BrowserModule,
+        AppRoutingModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
 })
 export class AppModule { }
